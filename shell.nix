caa95764f76117277edfb3da31d674cc26f79fbb
Chris Hodapp <hodapp87@gmail.com>
2017-12-27 17:16:04 -0500

    Added nltk & requests (needed by gensim) to shell.nix

--- shell.nix
+++ shell.nix
@@ -4,22 +4,18 @@ let stdenv = pkgs.stdenv;
     optional = stdenv.lib.optional;
     # N.B. Jupyter and TensorFlow can only coexist on Python 3.6, at
     # least on latest nixpkgs.
-    tf = pkgs.python36Packages.tensorflowWithCuda;
-    #tf = pkgs.python35Packages.tensorflow;
-    #tf_objdet = pkgs.python35Packages.tensorflow_object_detection.override {
-    #    tensorflow = tf;
-    #};
+    #tf = pkgs.python36Packages.tensorflowWithCuda;
+    tf = pkgs.python36Packages.tensorflow;
     python_with_deps = pkgs.python36.withPackages
       (ps: [ps.scipy tf ps.matplotlib ps.pandas ps.scikitlearn ps.h5py
             ps.Keras
             ps.easydict ps.pillow ps.pyyaml
             ps.pyqt4 # Needed only for matplotlib backend
             ps.pycallgraph ps.graphviz
-            ps.jupyter ps.gensim
-            #tf_objdet
+            ps.jupyter ps.gensim ps.nltk ps.requests
             ]);
 in stdenv.mkDerivation rec {
-  name = "yazabi-tensorflow-skill-test";
+  name = "sharpestminds-nlp-skill-test";
 
   buildInputs = with pkgs; [ python_with_deps ];
 }
