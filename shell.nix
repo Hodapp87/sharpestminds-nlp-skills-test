cf417679722a4c37fbf509dd40adab6d34efbdc4
Chris Hodapp <hodapp87@gmail.com>
2017-12-27 17:02:21 -0500

    Initial shell.nix commit

---
+++ shell.nix
@@ -0,0 +1,26 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let stdenv = pkgs.stdenv;
+    optional = stdenv.lib.optional;
+    # N.B. Jupyter and TensorFlow can only coexist on Python 3.6, at
+    # least on latest nixpkgs.
+    tf = pkgs.python36Packages.tensorflowWithCuda;
+    #tf = pkgs.python35Packages.tensorflow;
+    #tf_objdet = pkgs.python35Packages.tensorflow_object_detection.override {
+    #    tensorflow = tf;
+    #};
+    python_with_deps = pkgs.python36.withPackages
+      (ps: [ps.scipy tf ps.matplotlib ps.pandas ps.scikitlearn ps.h5py
+            ps.Keras
+            ps.easydict ps.pillow ps.pyyaml
+            ps.pyqt4 # Needed only for matplotlib backend
+            ps.pycallgraph ps.graphviz
+            ps.jupyter ps.gensim
+            #tf_objdet
+            ]);
+in stdenv.mkDerivation rec {
+  name = "yazabi-tensorflow-skill-test";
+
+  buildInputs = with pkgs; [ python_with_deps ];
+}
+
